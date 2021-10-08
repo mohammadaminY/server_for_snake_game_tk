61658aa72d5d566f4a8ead15b702450431508de3
mohammadamin <mohammadamin>
2021-10-08 15:04:23 +0330

    add best_score app to INSTALLED_APPS

--- server_for_snake_game_tk/settings.py
+++ server_for_snake_game_tk/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages', 'score',
+    'pages', 'best_score',
     'rest_framework', 'djoser'
 ]
 
