f3cd975bb53662abe689b7fd44aca7910d696840
mohammadamin <mohammadamin>
2021-10-08 14:46:21 +0330

    Create score app and delete accounts and some changes

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('login', views.login_view, name='login'),
-    path('login', views.signup_view, name='signup'),
-    path('login', views.logout_view, name='logout'),
-]
--- server_for_snake_game_tk/urls.py
+++ server_for_snake_game_tk/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/v1/score/', include('score.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-
 ]
