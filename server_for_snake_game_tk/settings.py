1e13ab3becf71546389f07c64d6467d72747286f
mohammadamin <mohammadamin>
2021-10-08 14:52:37 +0330

    Add REST_FRAMEWORK variable to settings.py

--- server_for_snake_game_tk/settings.py
+++ server_for_snake_game_tk/settings.py
@@ -119,3 +119,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.authentications.BasicAuthentication',
+        'rest_framework.authentications.SessionAuthentication',
+        'rest_framework.authentications.TokenAuthentication'
+    ]
+}
